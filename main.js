84feb1103d3ca82afd32ff64a0467e4a497d9696
KurachTaras <moonlight.jst@gmail.com>
2022-07-25 11:51:06 +0300

    Js lesson_1

---
+++ main.js
@@ -0,0 +1,117 @@
+// ---- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+
+
+
+// let str = "hello";
+// let company = "owu";
+// let web = "com";
+// let country = "ua";
+// let num = "1";
+// let num2 = "10";
+// let num3 = "-999";
+// let num4 = "123";
+// let num5 = "3.14";
+// let num6 = "2.5";
+// let num7 = "16";
+// let boolean = "true";
+// let boolean2 = "false";
+
+
+
+
+// ---Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// console.log(str);
+// console.log(company);
+// console.log(web);
+// console.log(country);
+// console.log(num);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(num5);
+// console.log(num6);
+// console.log(num7);
+// console.log(boolean);
+// console.log(boolean2);
+//
+// alert(str);
+// alert(company);
+// alert(web);
+// alert(country);
+// alert(num);
+// alert(num2);
+// alert(num3);
+// alert(num4);
+// alert(num5);
+// alert(num6);
+// alert(num7);
+// alert(boolean);
+// alert(boolean2);
+//
+// document.write(`
+// <div>${str}</div>
+// <hr>
+// <div>${company}</div>
+// <hr>
+// <div>${web}</div>
+// <hr>
+// <div>${country}</div>
+// <hr>
+// <div>${num}</div>
+// <hr>;
+// <div>${num2}</div>
+// <hr>
+// <div>${num3}</div>
+// <hr>
+// <div>${num4}</div>
+// <hr>
+// <div>${num5}</div>
+// <hr>
+// <div>${num6}</div>
+// <hr>
+// <div>${num7}</div>
+// <hr>
+// <div>${boolean}</div>
+// <hr>
+// <div>${boolean2}</div>
+// `);
+
+
+
+
+
+// ---- - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+// let firstname = 'Taras';
+// let middleName = 'Igorovich';
+// let lastName = 'Kurach';
+//
+// let person = (`${firstname} ${middleName} ${lastName}`);
+// console.log(person);
+
+
+
+
+
+// ---- - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+
+
+
+// ----- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+// let firstName = prompt("Your name?");
+// let middleName = prompt("Your middleName?");
+// let age = prompt("Your age?");
+//
+// alert(`${firstName} ${middleName} ${age}`);
